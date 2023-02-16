c97fbf5511a6029938dc9663e1f2cc991c7b8552
harrison056 <costa.silva.cs.cs@gmail.com>
2023-02-16 00:51:29 -0300

    final

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // config inicial
+require('dotenv').config()
 const express = require('express')
-const app = express()
-
-// depois do db
 const mongoose = require('mongoose')
+mongoose.set("strictQuery", true);
 
-const Person = require('./models/Person')
+const app = express()
 
 app.use(
   express.urlencoded({
@@ -15,103 +14,20 @@ app.use(
 
 app.use(express.json())
 
-// rotas
-app.post('/person', async (req, res) => {
-  const { name, salary, approved } = req.body
-
-  const person = {
-    name,
-    salary,
-    approved,
-  }
-
-  try {
-    await Person.create(person)
-
-    res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso!' })
-  } catch (error) {
-    res.status(500).json({ erro: error })
-  }
-})
-
-app.get('/person', async (req, res) => {
-  try {
-    const people = await Person.find()
-
-    res.status(200).json(people)
-  } catch (error) {
-    res.status(500).json({ erro: error })
-  }
-})
-
-app.get('/person/:id', async (req, res) => {
-  const id = req.params.id
-
-  try {
-    const person = await Person.findOne({ _id: id })
-
-    if (!person) {
-      res.status(422).json({ message: 'Usuário não encontrado!' })
-      return
-    }
-
-    res.status(200).json(person)
-  } catch (error) {
-    res.status(500).json({ erro: error })
-  }
-})
-
-app.patch('/person/:id', async (req, res) => {
-  const id = req.params.id
-
-  const { name, salary, approved } = req.body
-
-  const person = {
-    name,
-    salary,
-    approved,
-  }
-
-  try {
-    const updatedPerson = await Person.updateOne({ _id: id }, person)
-
-    if (updatedPerson.matchedCount === 0) {
-      res.status(422).json({ message: 'Usuário não encontrado!' })
-      return
-    }
-
-    res.status(200).json(person)
-  } catch (error) {
-    res.status(500).json({ erro: error })
-  }
-})
-
-app.delete('/person/:id', async (req, res) => {
-  const id = req.params.id
-
-  const person = await Person.findOne({ _id: id })
-
-  if (!person) {
-    res.status(422).json({ message: 'Usuário não encontrado!' })
-    return
-  }
-
-  try {
-    await Person.deleteOne({ _id: id })
-
-    res.status(200).json({ message: 'Usuário removido com sucesso!' })
-  } catch (error) {
-    res.status(500).json({ erro: error })
-  }
-})
+//rotas
+const routePerson = require('./routes/routePerson')
+app.use('/person', routePerson)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Oi Express!' })
 })
 
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
 mongoose
   .connect(
-    'mongodb+srv://user:password@restfulapibanco.lq7ds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    `mongodb+srv://${ DB_USER }:${ DB_PASSWORD }@cluster0.qxhns7y.mongodb.net/?retryWrites=true&w=majority`,
   )
   .then(() => {
     console.log('Conectou ao banco!')
